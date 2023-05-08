5764600ba59fce62113e88325d9cb188b5fad048
thalieskoda <thalieskoda@hotmail.com>
2023-05-08 17:07:51 -0400

    trying auth0

--- App.js
+++ App.js
@@ -1,149 +1,47 @@
 import React, { useState, useEffect } from "react";
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  FlatList,
-} from "react-native";
-
-//supabase
-import { supabase } from "./supabase/supabase";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 //components
-import ToDoItems from "./component/ToDoItems";
-import ModalComponent from "./component/ModalComponent";
+import supabase from "./supabase/supabase";
+import HomeScreen from "./component/HomeScreen";
+import LoginScreen from "./component/LoginScreen";
 
 const App = () => {
-  //Setting the states
-  const [items, setItems] = useState([]);
-  const [checkedItems, setCheckedItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [showModal, setShowModal] = useState(false);
-
-  //Setting up supabase with the 'Items' table
-  const getItems = async () => {
-    let { data: Items, error } = await supabase.from("Items").select("*");
-
-    return Items;
-  };
-  //Add New Item button
-  const addNewItem = async (item) => {
-    const { data: Items, error } = await supabase
-      .from("Items")
-      .insert([{ description: item, completed: false }]);
-
-    return Items;
-  };
-
-  //Save the new created item in the array
-  const saveItem = (item) => {
-    addNewItem(item).then(() => {
-      getItems().then((items) => {
-        //set items after the collection
-        setItems(items);
-      });
-    });
-  };
+  const [session, setSession] = useState(null);
 
   useEffect(() => {
-    getItems().then((items) => {
-      //set items after the collection
-      setItems(items);
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
     });
-  }, [isLoading]); //Everytime the loading state changes, it renders the page
 
-  return (
-    <>
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+  } else {
+    return (
       <SafeAreaView style={styles.mainView}>
-        <View style={styles.scrollView}>
-          {items.length > 0 ? (
-            <Text style={styles.heading}>My to do's</Text>
-          ) : (
-            <Text style={styles.heading}>Enter your first task below!</Text>
-          )}
-          {/*  To do list  */}
-          {items ? (
-            <FlatList
-              data={items}
-              renderItem={({ item, index }) => (
-                <ToDoItems
-                  setItems={setItems}
-                  item={item}
-                  index={index}
-                  checkedItems={checkedItems}
-                  isLoading={isLoading}
-                  setIsLoading={setIsLoading}
-                  items={items}
-                />
-              )}
-              keyExtractor={(item) => item.id.toString()}
-            />
-          ) : null}
-
-          {/* add new item */}
-          <TouchableOpacity
-            style={styles.addButton}
-            onPress={() => setShowModal(true)}
-          >
-            <Text style={styles.addItem}>add item</Text>
-          </TouchableOpacity>
+        <View>
+          <LoginScreen/>
+          <HomeScreen />
         </View>
-        {/* Add new item view */}
-
-        {showModal ? (
-          <ModalComponent
-            saveNewItem={saveItem}
-            hideModal={() => setShowModal(false)}
-          />
-        ) : null}
       </SafeAreaView>
-    </>
-  );
+    );
+  }
 };
 
 const styles = StyleSheet.create({
   mainView: {
     flex: 1,
-    backgroundColor: "#F8F9FD",
-  },
-  scrollView: {
-    flex: 1,
-    backgroundColor: "#F8F9FD",
-    padding: 15,
-  },
-  heading: {
-    fontSize: 30,
-    fontWeight: "bold",
-    marginTop: 60,
-    marginBottom: 30,
-  },
-  addButton: {
-    width: 77,
-    height: 77,
-    backgroundColor: "pink",
-    borderRadius: 10,
-    display: "flex",
-    bottom: 0,
-    right: 0,
-    position: "absolute",
-    justifyContent: "center",
-    alignItems: "center",
-    marginRight: 15,
-    marginBottom: 20,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-    shadowOpacity: 0.32,
-    shadowRadius: 5.46,
-
-    elevation: 9,
-  },
-  addItem: {
-    fontWeight: "200",
   },
 });
 
--- supabase/supabase.js
+++ supabase/supabase.js
@@ -1,8 +1,7 @@
 import 'react-native-url-polyfill/auto';
 import {createClient} from '@supabase/supabase-js';
+import { PUBLIC_API_KEY_SUPABASE, PUBLIC_URL_SUPABASE } from '@env';
 
-const supabaseUrl = 'https://lkkxckjqdkyzpgxousce.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxra3hja2pxZGt5enBneG91c2NlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMxNDI3NTMsImV4cCI6MTk5ODcxODc1M30.D0rxgJJM-Cc_-00KqkXGn-PL5lMWAg42qQIZD8j9nb0';
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(PUBLIC_URL_SUPABASE, PUBLIC_API_KEY_SUPABASE);
 
+export default supabase;
